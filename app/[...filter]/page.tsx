e40f38e87d0006674cdcf873924ca5a7718004c9
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2023-08-01 04:51:33 -0300

    changed headlines names

--- app/[...filter]/page.tsx
+++ app/[...filter]/page.tsx
@@ -4,11 +4,7 @@ const Category = ({ params }: { params: { filter: string[] } }) => {
   const country = params.filter[0];
   const category = params.filter[1];
 
-  if (country === "wd") {
-    return <ArticlesList category={category} />;
-  } else {
     return <ArticlesList country={country} category={category} />;
-  }
 };
 
 export default Category;
--- components/Articles/ArticlesList.tsx
+++ components/Articles/ArticlesList.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { Article } from "@/models";
 import { getNews } from "@/utils/getData";
 import ArticleCard from "./ArticleCard";
+import countries from "@/data/countries.json";
 
 const ArticlesList = ({
   query = "",
@@ -16,12 +17,18 @@ const ArticlesList = ({
   category?: string;
 }) => {
   const [articles, setArticles] = useState<Article[]>([]);
+  const [countryName, setCountryName] = useState("");
 
   useEffect(() => {
     const getArticles = async () => {
+      if (country === "wd") country = "";
       const articles = await getNews(query, country, category);
       setArticles(articles);
     };
+
+    setCountryName(
+      countries.filter((countryObj) => countryObj.alpha2 === country)[0].name
+    );
 
     getArticles();
   }, [query, country, category]);
@@ -32,7 +39,8 @@ const ArticlesList = ({
   return (
     <div className="xl mx-4 mt-4 grid max-w-6xl gap-x-4 gap-y-4 sm:mx-8 md:mx-16 lg:grid-cols-2 xl:mx-auto">
       <h2 className="col-span-full text-xl font-medium " data-test="heading">
-        {country} {category === "general" ? "Top" : categoryUppercase} Headlines
+        {countryName} {category === "general" ? "Top" : categoryUppercase}{" "}
+        Headlines
       </h2>
       {articles?.map((article) => (
         <ArticleCard key={article.url} article={article} />
